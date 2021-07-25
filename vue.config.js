4169517d59c4838d8f9e52c77470f90144f81266
SepVeneto <sepveneto@qq.com>
2021-07-25 17:23:15 +0800

    静态站点部署

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,35 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const path = require('path');
 
+function parseExternals(config) {
+  return config.reduce((ext, curr) => {
+    ext[curr.name] = curr.scope;
+    return ext;
+  }, {})
+}
+
+const cdnConfig = process.env.NODE_ENV === 'production' ? [
+  {
+    name: 'vue',
+    scope: 'Vue',
+    js: 'https://cdn.jsdelivr.net/npm/vue@2.6.14',
+  },
+  {
+    name: 'element-ui',
+    scope: 'ELEMENT',
+    js: 'https://cdn.jsdelivr.net/npm/element-ui@2.15.3/lib/index.js',
+    css: 'https://cdn.jsdelivr.net/npm/elementgui@2.15.3/lib/theme-chalk/index.css'
+  }
+] : [];
+
 module.exports = {
-  publicPath: '/static/dynamicForm/', // 静态资源的公开路径
+  publicPath: process.env.NODE_ENV === 'production' ? '/dynamic-form-making/' : '', // 静态资源的公开路径
   chainWebpack(config) {
+    config.plugin('html').tap(args => {
+      args[0].title = 'dynamic-form-making';
+      args[0].cdnConfig = cdnConfig;
+      return args;
+    })
     config.module.rule('svg').exclude.add(path.join(__dirname, 'src/icons')).end();
     config.module.rule('icons')
       .test(/\.svg$/)
@@ -17,8 +43,9 @@ module.exports = {
       .end();
   },
   configureWebpack: {
-    plugins: [
-      new BundleAnalyzerPlugin()
-    ]
+    externals: parseExternals(cdnConfig),
+    // plugins: [
+    //   new BundleAnalyzerPlugin()
+    // ]
   }
 }
